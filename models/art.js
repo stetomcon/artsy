3496044affb238d5445633db0788dbb1d4ff80c0
Steven Wolanin Villanueva <stevencontreras@stevens-MacBook.local>
2019-11-12 10:24:11 -0500

    final

--- models/art.js
+++ models/art.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const artSchema = new mongoose.Schema({
     title: { type: String, required: true, maxlength: 7 },
     img: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String },
     tag: { type: [String] },
+    comments: { type: String },
 });
 
 const art = mongoose.model('art', artSchema);
